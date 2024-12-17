add2a253520084d6379f3c6b20e15d50f4bf6502
Metwally_22 <mahmoudmetwally9548@gmail.com>
2024-12-18 00:08:57 +0200

    C43-G03-C#05

---
+++ Dimo5/Program.cs
@@ -0,0 +1,148 @@
+﻿namespace Dimo5
+{
+    internal class Program
+    {
+        //public static void swap(ref int x,ref int y)
+        //{ 
+        // int temp=x; x=y; y=temp;
+        //}
+        //public static void project(string pattern, int count)
+        //{
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        Console.WriteLine("*");
+        //    }
+        //}
+        //public static int pet(ref int[] Arr)
+        //{
+        //    int sum=0;
+
+        //    if (Arr != null)
+        //    {
+        //        for (int i = 0; i < Arr.Length; i++)
+        //        {
+        //            sum += Arr[i];
+        //        }
+        //    }
+        //    return sum;
+        //}
+        #region params
+        //public static int sumarray( params int[] Arr)
+        //    {
+        //    int sum = 0;
+        //        if (Arr != null)
+        //            {
+        //             for (int i = 0; i < Arr.Length; i++)
+        //             sum += Arr[i];                      
+        //            }
+        //        return sum;
+        //    }
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            #region passing ref by value
+            //int x = 3;
+            //int y = 4;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //swap(ref x,ref y);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            #endregion
+            #region ref by type
+            //int[] numbers = { 1, 2, 3};
+            //int result=pet(ref numbers);
+            //Console.WriteLine(result);
+
+            #endregion
+            #region passing by out
+            //int a = 3, b = 4, resultsm ,  resultmul ;
+            //summul(a,b,out resultsm ,out resultmul);
+            //Console.WriteLine($"sum{resultsm}");
+            //Console.WriteLine($"mul{resultmul}");
+
+
+            #endregion
+            #region params
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int sum = sumarray(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            //Console.WriteLine($"sum of number {sum}");
+            #endregion
+            #region boxing & boxing
+            //int x = 10;
+            //object y = x;  
+            //Console.WriteLine(y);
+            ////boxing 
+            ////casting from value type to referance type {safe casting}
+
+            ////---------------unboxing---------------
+            //object z = 10;
+            //z = "mahmoud";
+            //int xr = (int)z;
+            //Console.WriteLine(xr);
+            ////unboxing
+            ////casting from referance type to value type {unsafe casting}
+
+
+            #endregion
+            #region nullable code
+            //int? number; // Nullable<int>number
+            //number = 3;
+            //number = null;
+
+            ////----------double---------------
+            //double? number2; // Nullable<double>number2;
+            //number2 = 48;
+
+            //-----------null forgiveness--------------------
+
+            //number2 = null;
+            //string name = "mah";
+            //name = null;
+            //string? name2=null;
+            //Console.WriteLine(name);
+            //Console.WriteLine(name2);
+
+            //string notnull = "";
+            //string? nallable=null;
+            //notnull = nallable!; //null forgiveness {!}
+
+
+
+            #endregion
+            #region how work nullable
+            //int x = 10;
+            //int? y = 20;
+            //y = null;
+            //y = x;
+
+            // -----------------*unboxing*------------
+            //int? x = 20;
+            //x = null;
+            //int y = 30;
+            //y = (int)x;
+            // y = x.HasValue ? x.Value : y;
+            //y = x ?? 0;
+
+            #endregion
+            #region null propagation
+            //double x = default;
+            //int[]? numbers = default;
+
+            //if (numbers != null)
+            //{ 
+            // for (int i = 0; i < numbers?.Length; i++)
+            //    {
+            //        x = numbers[i];
+            //    }
+
+            //}
+
+            //numbers != null ? numbers.Length : 0; // null propagation
+            //int length = numbers?.Length ?? 0; // coleasing 
+            #endregion
+        }
+    }
+}
---
+++ assignment_5/Program.cs
@@ -0,0 +1,159 @@
+﻿namespace assignment_5
+{
+    internal class Program
+    {
+        #region ----1
+        //public static void value(ref int x, ref int y)
+        //{
+        //    int num = x;
+        //    x = y;
+        //    y = num;
+        //}
+        #endregion
+        #region ----2
+        //public static int nums(int[] Arr)
+        //{ 
+        // int sum=0;
+        //    for (int i = 0; i < Arr.Length; i++)
+        //    { 
+        //     sum += Arr[i];
+        //    }
+        //    return sum;
+        //}
+        #endregion
+        #region ----3
+        //public static int sumsup(int x, int y, out int sum, out int sup)
+        //{
+        //    sum = x + y;
+        //    sup = x-y;
+        //    return 0;
+        //}
+
+        #endregion
+        #region -----4
+        //public static int sum(int number)
+        //{
+        //    int sum = 0;
+
+        //    while (number > 0)
+        //    {
+        //        int digit = number % 10;
+
+        //        sum += digit;
+
+        //        number /= 10;
+        //    }
+
+        //    return sum;
+        //}
+        #endregion
+        #region -----5
+        //public static bool IsPrime(int number)
+        //{
+        //    if (number <= 1)
+        //    {
+        //        return false;
+        //    }
+
+        //    if (number % 2 == 0 && number != 2)
+        //    {
+        //        return false;
+        //    }
+
+        //    for (int i = 3; i * i <= number; i += 2)
+        //    {
+        //        if (number % i == 0)
+        //        {
+        //            return false;
+        //        }
+        //    }
+
+        //    return true;
+        #endregion
+        #region -----6
+        //public static void MinMaxArray(int[] arr, out int min, out int max)
+        //{
+        //    if (arr.Length == 0)
+        //    {
+        //        throw new ArgumentException("Array is empty.");
+        //    }
+
+        //    min = arr[0];
+        //    max = arr[0];
+
+        //    for (int i = 1; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] < min)
+        //        {
+        //            min = arr[i];
+        //        }
+
+        //        if (arr[i] > max)
+        //        {
+        //            max = arr[i];
+        //        }
+        //    }
+        //}
+            #endregion
+            static void Main(string[] args)
+        {
+            #region -----1 ref by value
+            ////befor swap
+            //int x = 4 , y = 6 ;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            ////after swap
+            //value(ref x,ref y);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            #endregion
+            #region -----2 ref by type
+            //int[] numbers = { 1, 2, 3, 4, 5, 6};
+            //int achion = nums(numbers);
+            //Console.WriteLine($"this is nums of ref type {achion}");
+            #endregion
+            #region -----3
+            //int x = 5;
+            //int y = 7;
+            //int sum, sup;
+            //Console.WriteLine($"sum {x+y}");
+            //Console.WriteLine($"sup {x-y}");
+            #endregion
+            #region -----4
+            //Console.Write("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //int sumDigits = sum(num);
+
+            //Console.WriteLine("The sum of the digits of the number {0} is: {1}", num, sumDigits);
+            #endregion
+            #region -----5
+            //Console.Write("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //if (IsPrime(num))
+            //{
+            //    Console.WriteLine($"{num} is a prime number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{num} is not a prime number.");
+            #endregion
+            #region -----6
+            //int[] numbers = { 5, 2, 9, 1, 5, 6 };
+            //int min, max;
+
+            //MinMaxArray(numbers, out min, out max);
+
+            //Console.WriteLine($"Minimum value: {min}");
+            //Console.WriteLine($"Maximum value: {max}");
+            #endregion
+            #region ----7
+
+            #endregion
+
+        }
+    
+    }
+}
